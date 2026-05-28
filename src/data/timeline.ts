39fb759435016ffa09285a024d8c9063b07fff48
Mohammed Ateeq Ur Rehman <39751796+x-claimer@users.noreply.github.com>
2026-05-27 22:28:00 -0400

    added resume button and fixed NOW page

---
+++ src/data/timeline.ts
@@ -0,0 +1,23 @@
+export interface TimelineEntry {
+  date: string;
+  title: string;
+  description?: string;
+}
+
+export const timeline: TimelineEntry[] = [
+  {
+    date: "May 2025",
+    title: "Graduated with Master's Degree in Data Science",
+    description: "Completed my Master's in Data Science at the University of Maryland.",
+  },
+  {
+    date: "Aug 2020 - March 2024",
+    title: "Associate Consultant(Data Engineer) at Capgemini",
+    description: "Worked as a Associate Consultant(Data Engineer) at Capgemini.",
+  },
+  {
+    date: "June 2020",
+    title: "Graduated with Bachelor's Degree in Electronics and Communication Engineering",
+    description: "Completed my Bachelor's in Electronics and Communication Engineering at the NIT Calicut.",
+  },
+];
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,7 @@ export const LINKS = {
   github: "https://github.com/x-claimer",
   linkedin: "https://www.linkedin.com/in/mohammedateequrrehman/",
   email: "mailto:ateeq13.rehman@gmail.com",
+  resume: "https://drive.google.com/file/d/1Zhd6svV5aXXFz3HL884LEnkD8OtMK-vA/view?usp=sharing",
 };
 
 export const loaderAnimation = [

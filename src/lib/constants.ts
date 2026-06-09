004de4b8f79c450977ec77422e49eac6614354b9
Mohammed Ateeq Ur Rehman <39751796+x-claimer@users.noreply.github.com>
2026-06-09 14:43:33 -0400

    resume update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const LINKS = {
   github: "https://github.com/x-claimer",
   linkedin: "https://www.linkedin.com/in/mohammedateequrrehman/",
   email: "mailto:ateeq13.rehman@gmail.com",
-  resume: "https://drive.google.com/file/d/1Zhd6svV5aXXFz3HL884LEnkD8OtMK-vA/view?usp=sharing",
+  resume: "https://github.com/x-claimer/Ateeq-Portfolio/blob/main/src/data/Final_Job_Resume_draft_one_page_new.pdf",
 };
 
 export const loaderAnimation = [
